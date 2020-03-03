4cacf5714c697b576d92d2a8502b7e33bb069157
tahirs95 <tahirs95@hotmail.com>
2020-03-03 13:52:21 +0500

    Fixation of bug.

--- thirtyworks/blog/cron.py
+++ thirtyworks/blog/cron.py
@@ -9,7 +9,7 @@ def email(subject, message, recipient_list):
     email_from = settings.EMAIL_HOST_USER
     send_mail( subject, message, email_from, recipient_list )
 
-@kronos.register('0 0 * * *')
+@kronos.register('30 13 * * *')
 def complain():
     rejected_users = []
     accepted_users = []
@@ -19,14 +19,17 @@ def complain():
     for post in posts:
         authors.append(post.author.username)
     users = UserProfile.objects.all()
+    print(authors)
     for user in users:
-        if user.user.username not in author and user.user.is_active:
-            rejected_users.append(user.user.email)
-            user.user.is_active = False
-            user.blocked = True
-            user.save()
-        else:
-            accepted_users.append(user.user.email)
+        if user.user.is_active and not user.user.is_staff:
+            if user.user.username not in authors:
+                rejected_users.append(user.user.email)
+                user.user.is_active = False
+                user.user.save()
+                user.blocked = True
+                user.save()
+            else:
+                accepted_users.append(user.user.email)
     day_number = latest_day.number + 1
     day = Day(number=day_number)
     day.save()
@@ -34,7 +37,12 @@ def complain():
     accepted_message = "Your task is to play."
     rejected_subject = "Rejected."
     rejected_message = "You are being blocked to use the system."
+    print(accepted_users)
+    print(rejected_users)
     email(rejected_subject, rejected_message, rejected_users)
+    print("Email has been sent to rejected users.")
     email(accepted_subject, accepted_message, accepted_users)
+    print("Email has been sent to active users.")
 
-#  python manage.py installtasks
+# python manage.py installtasks
+# python manage.py showtasks
