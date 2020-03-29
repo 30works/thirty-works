5ff775010511d25c30b6fa3d0deb35548e851909
Xavier <xavierdcruz0@gmail.com>
2020-03-29 14:59:57 +0100

    filter by blocked user, create user script

--- thirtyworks/users/admin.py
+++ thirtyworks/users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import UserProfile
+from django_admin_listfilter_dropdown.filters import DropdownFilter, RelatedDropdownFilter, ChoiceDropdownFilter
 
 
 # Register your models here.
-admin.site.register(UserProfile)
+# admin.site.register(UserProfile)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_filter = ['blocked']
+
+admin.site.register(UserProfile, UserProfileAdmin)
